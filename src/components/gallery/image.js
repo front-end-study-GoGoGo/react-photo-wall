8818d754c4fe503212e6d61ea3b219403d974cd8
zhangqiang <zhangq@yyits.cn>
2021-12-01 22:19:29 +0800

    Revert "update;"
    This reverts commit ca4feaeff954539241cfe5008fb254dc6245568d.

---
+++ src/components/gallery/image.js
@@ -0,0 +1,60 @@
+import React, { Component, useState } from 'react'
+
+class Image extends Component {
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     styleObj: {}
+  //   };
+  // }
+
+  handleClick(e) {
+    if (this.props.arrange.isCenter) {
+      console.log(111);
+      this.props.reverse()
+    } else {
+      console.log(222)
+      this.props.center()
+    }
+
+    e.preventDefault()
+  }
+
+  render() {
+    let styleObj = {}
+    if (this.props.arrange.pos) {
+      console.log('this.props.arrange.pos:', this.props.arrange.pos);
+      styleObj = this.props.arrange.pos;
+    }
+    // console.log('this.props.arrange.rotate:', this.props.arrange.rotate);
+    // if (this.props.arrange.rotate) {
+    //   styleObj["transform"] = `rotate(${this.props.arrange.rotate}deg)`
+    // }
+    // if (this.props.arrange.isCenter) {
+    //   styleObj['zIndex'] = 11;
+    // }
+    let figureClassName = "img-figure"
+    figureClassName += this.props.arrange.isReverse ? ' is-reverse' : ''
+    // this.setState({ styleObj: 1 });
+
+    return (
+      <figure
+        className={figureClassName} id={this.props.id}
+        style={styleObj}
+        onClick={this.handleClick.bind(this)}
+      >
+        <div className="front">
+          <img src={this.props.data.url} alt={this.props.data.title} />
+          <h3 className="img-title">{this.props.data.title}</h3>
+        </div>
+        <div className="back" onClick={this.handleClick.bind(this)}>
+          <p>
+            {this.props.data.desc}
+          </p>
+        </div>
+      </figure>
+    )
+  }
+}
+export default Image
